4d957376ec61f69c82c8f27c4e9f8c4be617599b
Sarah-Rose Prince <sarah-roseprince@Sarah-Roses-MacBook-Pro.local>
2023-01-30 19:27:20 -0600

    Fixed try/except...hopefully

--- payCalculator.py
+++ payCalculator.py
@@ -1,16 +1,24 @@
 def calculatePay():
     # This first line is provided for you
-    try:
-        hrs = float(input("Enter Hours: "))
-        rate = float(input("Enter Rate: "))
-        ot_rate = rate * 1.5 
-        if hrs <= 40:
-         print('Pay:', (hrs * rate))
-        else:
-         ot_hrs = hrs - 40
-         print('Pay:', ((ot_hrs * ot_rate) + (rate * 40)))
+    hrs = input('Enter Hours: ')
+    try: 
+        hrs = float(hrs)
     except:
         print('Please enter a number')
+
+    rate = input('Enter Rate: ')
+    try:
+        rate = float(rate)
+    except:
+        print('Please enter a number')
+
+    ot_rate = rate *1.5    
+    if hrs <= 40:
+        print('Pay:', (hrs * rate))
+    else:
+        ot_hrs = hrs - 40
+        print('Pay:', ((ot_hrs * ot_rate) + (rate * 40)))
+
     # end assignment
 
 ## if you want to test locally before you try to sync
